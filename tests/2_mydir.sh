3afa55523044e32ef4d04661c4b84c840a35fdae
xonix <xonixx@gmail.com>
2021-01-10 04:02:43 +0200

    work on define overrides

--- tests/2_mydir.sh
+++ tests/2_mydir.sh
@@ -32,3 +32,26 @@
 @goal mydir_in_required_if_6
 @reached_if [[ ! -f "$MYDIR/2_mydir.txt" ]]
   echo "should show"
+
+@goal mydir_in_reached_if_of_dep_1
+@depends_on dep_1
+  echo "should show"
+
+@goal mydir_in_reached_if_of_dep_2
+@depends_on dep_2
+  echo "should show"
+
+@goal dep_1
+@depends_on dep_3
+#@reached_if echo "#$MYDIR#"; [[ -f "$MYDIR/2_mydir.txt" ]]
+@reached_if [[ -f "$MYDIR/2_mydir.txt" ]]
+  echo "should not show"
+
+@goal dep_2
+@depends_on dep_3
+#@reached_if echo "#$MYDIR#"; [[ ! -f "$MYDIR/2_mydir.txt" ]]
+@reached_if [[ ! -f "$MYDIR/2_mydir.txt" ]]
+  echo "should show"
+
+@goal dep_3
+  echo dep_3
