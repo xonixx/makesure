4585af8901d958b863a342dc0238a9edef4775eb
xonix <xonixx@gmail.com>
2021-01-26 18:24:56 +0200

    fix for defines handling in @reached_if

--- tests/10_define.sh
+++ tests/10_define.sh
@@ -15,3 +15,26 @@ echo C=$C
 
 @goal testABC
 @depends_on testA testB testC
+
+@goal is_not_reached
+@depends_on other_goal_2
+@reached_if [[ "$A" == "XXX" ]]
+  echo "should show"
+
+@goal must_be_reached1
+@reached_if [[ "$A" == "aaa" ]]
+  echo "should not show"
+
+@goal must_be_reached2
+@depends_on other_goal_1
+@reached_if [[ "$B" == "aaabbb" ]]
+  echo "should not show"
+
+@goal other_goal_1
+  echo "other goal 1"
+
+@goal other_goal_2
+  echo "other goal 2"
+
+@goal children_reached_or_not
+@depends_on must_be_reached1 must_be_reached2 is_not_reached
