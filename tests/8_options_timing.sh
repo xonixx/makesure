c85b1367abcde1cf64381aace9ee78b160c9c19e
xonix <xonixx@gmail.com>
2021-01-26 19:12:30 +0200

    Do not measure timing for [already satisfied] and [empty] goals #6

--- tests/8_options_timing.sh
+++ tests/8_options_timing.sh
@@ -3,3 +3,19 @@
 
 @goal default
   echo test
+
+
+@goal reached_goal
+@reached_if true
+  echo "should not show"
+
+@goal not_reached_goal_1
+@reached_if false
+  echo "should show 1"
+
+@goal not_reached_goal_2
+  echo "should show 2"
+
+@goal empty_goal
+@depends_on reached_goal not_reached_goal_1 not_reached_goal_2
+
