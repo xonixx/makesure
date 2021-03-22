9f307a217e88ec21aa6a87cc2cce19d668cf601a
xonix <xonixx@gmail.com>
2021-03-22 19:09:58 +0200

    Propose a method to have parameterized goals #12

--- tests/11_call_good.sh
+++ tests/11_call_good.sh
@@ -1,7 +1,17 @@
 
+@define V="123"
 
 @goal a
 @call script1 world
 
+@goal b
+@call script1 $V
+
+@goal c
+@call script2 $MYDIR
+
 @script script1
   echo "Hello $1"
+
+@script script2
+  cat "$1/2_mydir.txt"
