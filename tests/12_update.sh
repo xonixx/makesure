c46d3bc7abe128c655ee3e01361bc641587d47ab
xonix <xonixx@gmail.com>
2021-08-01 18:04:38 +0300

    Add test for update scenario #21

--- tests/12_update.sh
+++ tests/12_update.sh
@@ -4,9 +4,16 @@
 @define makesure_stableXXX="$D/makesure_stableXXX"
 
 @goal test1
+  [[ -d "$D" ]] && rm -r "$D"
   mkdir "$D"
   awk '{ if (/^VERSION="/) print "VERSION=\"XXX\""; else print }' ../makesure_stable > "$makesure_stableXXX"
   chmod +x "$makesure_stableXXX"
+  ln -s `which which` "$D/which"
+  ln -s `which awk` "$D/awk"
+  ln -s `which mktemp` "$D/mktemp"
+  ln -s `which rm` "$D/rm"
+  ln -s `which cat` "$D/cat"
+  export PATH="$D"
   "$makesure_stableXXX" --version
   "$makesure_stableXXX" --selfupdate
   "$makesure_stableXXX" --version
