37a0fa83372841bf5a0f2d7858e5668c9500460e
xonix <xonixx@gmail.com>
2021-08-06 21:03:28 +0300

    Is it possible to make ./makesure --selfupdate to work on Win? #22

--- tests/12_update.sh
+++ tests/12_update.sh
@@ -42,21 +42,20 @@
   @depends_on run_selfupdate
 
 @goal wget_prepared
-  set -x
+#  set -x
   cmd="wget"
-  cmd1=`command -v $cmd 2>/dev/null`
-  echo "$cmd1 :: $?"
+#  echo "$cmd1 :: $?"
   (
     echo "#!/bin/bash"
     echo 'echo "running wget"'
-    if [[ -z $cmd1 ]]
+    if cmd1=`command -v $cmd 2>/dev/null`
     then
+      echo "exec $cmd1 \"\$@\""
+    else
       # fake wget with curl
 #      echo "set -x"
 #      echo 'echo "1=$1 2=$2 3=$3"'
       echo "exec $(command -v curl) \"\${1/-q/-s}\" \"\$2\" \"\${3/-O/-o}\""
-    else
-      echo "exec $cmd1 \"\$@\""
     fi
   ) > "$D/$cmd"
   chmod +x "$D/$cmd"
