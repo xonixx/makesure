1482515d91c5589bfae3fb887e4b8ffd2ccd3528
xonix <xonixx@gmail.com>
2021-09-05 18:13:25 +0300

    Revise variables behavior #44

--- tests/19_vars.sh
+++ tests/19_vars.sh
@@ -1,6 +1,11 @@
 
 @define V="prelude"
 
+echo "in prelude: A=$A"
+echo "in prelude: V=$V"
+sh -c 'echo "in prelude: in child: A=$A"'
+sh -c 'echo "in prelude: in child: V=$V"'
+
 @goal default
   echo "A=$A"
   echo "V=$V"
