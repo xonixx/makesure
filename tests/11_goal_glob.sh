c8d95e8b8661ed4f68016452e40f1c2e08492cf3
xonix <xonixx@gmail.com>
2021-09-19 17:47:02 +0300

    Improvement to `@goal @glob` #46

--- tests/11_goal_glob.sh
+++ tests/11_goal_glob.sh
@@ -14,8 +14,8 @@
 @depends_on 11_goal_glob_3.txt
 
 @goal test2
-@depends_on goal_name@11_goal_glob_2.txt
-@depends_on goal_name@11_goal_glob_3.txt
+@depends_on glob_goal_name@11_goal_glob_2.txt
+@depends_on glob_goal_name@11_goal_glob_3.txt
 
-@goal goal_name @glob 11_goal_glob*.txt
-  echo "goal_name ::: $ITEM :: $INDEX :: $TOTAL"
+@goal glob_goal_name @glob 11_goal_glob*.txt
+  echo "glob_goal_name ::: $ITEM :: $INDEX :: $TOTAL"
