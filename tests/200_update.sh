09980ca8086eba59b415896266a310017b9859c6
xonix <xonixx@gmail.com>
2021-12-12 16:42:03 +0200

    Run CI tests on FreeBSD #54

--- tests/200_update.sh
+++ tests/200_update.sh
@@ -67,7 +67,7 @@
   echo $'#!/bin/sh
 echo "running wget"
 # fake wget with curl
-exec awk -v CURL="'$(command -v curl)$'" -v a1="$1" -va2="$2" -v a3="$3" \'
+exec awk -v CURL="'$(command -v curl)$'" -v a1="$1" -v a2="$2" -v a3="$3" \'
 BEGIN {
 sub(/-q/,"-s",a1)
 sub(/-O/,"-o",a3)
