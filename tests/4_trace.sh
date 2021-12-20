779e1625d3b6a76e70038df5ed36e3bd271fb772
xonix <xonixx@gmail.com>
2021-12-20 18:28:58 +0200

    Tracing should trace all executed shell code #86

--- tests/4_trace.sh
+++ tests/4_trace.sh
@@ -2,4 +2,13 @@
 
 @goal default
 @reached_if [[ 1 -eq 2 ]]
+@depends_on goal1
   echo "A=$A"
+
+@goal goal1
+@depends_on goal2
+  echo 'goal 1'
+
+@goal goal2
+@reached_if true
+  echo 'should not show'
