1adf3537c8af4292e149039b21a08d9caeadd34f
xonix <xonixx@gmail.com>
2021-12-22 11:53:39 +0200

    Insufficient validation in `@define` #85

--- tests/24_define_validation.sh
+++ tests/24_define_validation.sh
@@ -1,27 +1,28 @@
 
 @define A=aaa
-@define B='bbb'
-@define C=$'cc\'c'
-@define D=a\ b\ c
-@define E="eee"
+@define B='bbb bbb'
+@define C=$'cc\'c  cc\'c'
+@define C1='cc'\''c  cc'\''c'
+@define D=a\ b\ \ c
+@define E="eee eee"
 
-@define K=aaa'bbb'$'cc\'c'a\ b\ c"eee"
-@define L="$A$B$C$D$E"
+@define K=aaa'bbb'$'cc\'c'a\ b\ \ c"eee"
+@define L="$A$B $C$D  $E"
 
-@define A1=aaa          # comment
-@define A2=aaa#         # comment
-@define B1='bbb'  # comment
-@define B2='bbb'#  # comment
-@define C1=$'cc\'c'  #
-@define D1=a\ b\ c #
-@define E1="eee"  # comment
+@define AC1=aaa          # comment
+@define AC2=aaa#         # comment
+@define BC1='bbb bbb'  # comment
+@define BC2='bbb bbb'#  # comment
+@define CC1=$'cc\'c  cc\'c'  #
+@define DC1=a\ b\ c #
+@define EC1="eee"  # comment
 
-@define K1=aaa'bbb'$'cc\'c'a\ b\ c"eee" # some comment
-@define L1="$A$B$C$D$E"                 # other comment
+@define KC1=aaa'bbb'$'cc\'c'a\ b\ c"eee" # some comment
+@define LC1="$A$B$C$D$E"                 # other comment
 
 @goal default
-  for x in A B C D E K L A1 A2 B1 B2 C1 D1 E1 K1 L1
+  for x in A B C C1 D E K L AC1 AC2 BC1 BC2 CC1 DC1 EC1 KC1 LC1
   do
-    echo "$x=${!x}"
+    printf '%-3s=%s\n' "$x" "${!x}"
   done
 
