3d75aef2a5e5acf1681cee83a9462e7b72824ad5
xonix <xonixx@gmail.com>
2021-12-22 12:48:28 +0200

    Insufficient validation in `@define` #85

--- tests/24_define_validation.sh
+++ tests/24_define_validation.sh
@@ -1,6 +1,10 @@
 
 @define A=aaa
 @define B='bbb bbb'
+@define B1='bbb # bbb'
+@define B2='bbb ; bbb'
+@define B3='bbb ; bbb';
+@define B4='bbb ; bbb'    ;
 @define C=$'cc\'c  cc\'c'
 @define C1='cc'\''c  cc'\''c'
 @define D=a\ b\ \ c
@@ -13,6 +17,8 @@
 @define AC2=aaa#         # comment
 @define BC1='bbb bbb'  # comment
 @define BC2='bbb bbb'#  # comment
+@define BC3='bbb bbb';  # comment
+@define BC4='bbb ; bbb'   ;  # comment
 @define CC1=$'cc\'c  cc\'c'  #
 @define DC1=a\ b\ c #
 @define EC1="eee"  # comment
@@ -21,7 +27,7 @@
 @define LC1="$A$B$C$D$E"                 # other comment
 
 @goal default
-  for x in A B C C1 D E K L AC1 AC2 BC1 BC2 CC1 DC1 EC1 KC1 LC1
+  for x in A B B1 B2 B3 B4 C C1 D E K L AC1 AC2 BC1 BC2 BC3 BC4 CC1 DC1 EC1 KC1 LC1
   do
     printf '%-3s=%s\n' "$x" "${!x}"
   done
--- tests/24_define_validation_error.sh
+++ tests/24_define_validation_error.sh
@@ -1,9 +1,22 @@
 
 @define echo
+@define echo;
+@define echo;echo
+@define echo ; echo
 @define echo 'Hello'
+@define echo 'Hello';
 @define A=aaa echo 'Hello'
 @define B=aaa\ aaa echo 'Hello'
 @define C='aaa aaa' echo 'Hello'
+
+@define echo                       # comment
+@define echo 'Hello'           # comment
+@define echo 'Hello';           # comment
+@define echo 'Hello'  ;# comment
+@define A=aaa echo 'Hello'        # comment
+@define B=aaa\ aaa echo 'Hello'# comment
+@define C='aaa aaa' echo 'Hello' # comment
+@define C='aaa aaa' echo 'Hello' ; # comment
 
 @goal default
   echo 'Should not show'
