e77916004754be8ed6e109d9edb95e7d61dee3e2
xonix <xonixx@gmail.com>
2022-02-11 17:10:04 +0200

    Auto-completion #75 : `expect`-based test for auto-completion

--- completion.bash
+++ completion.bash
@@ -6,11 +6,6 @@ _makesure_completions() {
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD - 1]}"
   cnt="${#COMP_WORDS[@]}"
-  #echo ">> $cur :: $prev :: $COMP_CWORD"
-
-  # DONE auto-complete goals
-  # DONE auto-complete options
-  # DONE auto-complete goals from correct file (-f)
 
   if [[ "$prev" == '-f' || "$prev" == '--file' ]]
   then
@@ -20,18 +15,12 @@ _makesure_completions() {
 
   # before we scan for targets, see if a Makesurefile name was
   # specified with -f/--file
-  for ((i = 1; i < ${#COMP_WORDS[@]}; i++)); do
+  for ((i = 1; i < cnt; i++)); do
       if [[ ${COMP_WORDS[i]} == '-f' || ${COMP_WORDS[i]} == '--file' ]]; then
-          # eval for tilde expansion
-#          echo
-#          printf 'f=%s\n' "${COMP_WORDS[i + 1]}"
           eval "makesurefile=${COMP_WORDS[i + 1]}"
           break
       fi
   done
-
-#  echo
-#  echo "makesurefile=$makesurefile"
 
   COMPREPLY=($(compgen -W "$(./makesure --file "$makesurefile" -la | awk -F: '
 BEGIN {
