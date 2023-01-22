96d17dbc9c18864c7f87f7188ad18dd1e3b0bdb2
xonix <xonixx@gmail.com>
2023-01-23 01:36:50 +0200

    Parameterized goals #115

---
+++ tests/27_parameterized_goals_10_errors.sh
@@ -0,0 +1,22 @@
+
+# all errors at once
+
+@goal a
+@depends_on b                    # err missing args
+@depends_on b @args              # err missing args
+@depends_on b @args 'hello' 'hi' # err more args than params
+@depends_on b @args WRONG1       # err unknown arg
+@depends_on e @args 'arg'        # err args for non-PG
+
+
+@goal b @params S
+@depends_on c @args S
+@depends_on c @args WRONG2       # err unknown arg
+  echo "$S world from b"
+
+@goal c @params S1
+@depends_on e @args WRONG3 WRONG4 # multiple errors
+  echo "$S1 world from c"
+
+@goal e
+  echo "e"
