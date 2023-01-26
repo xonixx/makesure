04f7397c2e62238f91e78f7c88cd43669154ca65
xonix <xonixx@gmail.com>
2023-01-26 22:17:13 +0200

    trying to troubleshoot issue with mac 10.15

--- tests/12_errors_1.sh
+++ tests/12_errors_1.sh
@@ -7,7 +7,9 @@
 #@goal @glob '*.txt' should not have anything after glob pattern
 #@goal @glob '*.txt' should_not_have_anything_after_glob_pattern
 
-@goal @glob '*.txt' # but comment is OK
+#@goal @glob '*.txt' # but comment is OK
 #
-#@goal @glob     # absent glob pattern
+
+@goal @glob     # absent glob pattern
+
 #@goal g19 @glob # absent glob pattern
