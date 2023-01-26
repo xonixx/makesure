a68332a4c8d7adadccacec97a11b97caedc3d58b
xonix <xonixx@gmail.com>
2023-01-26 22:14:41 +0200

    trying to troubleshoot issue with mac 10.15

--- tests/12_errors_1.sh
+++ tests/12_errors_1.sh
@@ -1,12 +1,13 @@
-@goal g15 should not have anything after goal name
-@goal g16 # but comment is OK
+#@goal g15 should not have anything after goal name
+#@goal g16 # but comment is OK
 
-@goal g17 @glob '*.txt' should not have anything after glob pattern
+#@goal g17 @glob '*.txt' should not have anything after glob pattern
 @goal g18 @glob '*.txt' # but comment is OK
 
-@goal @glob '*.txt' should not have anything after glob pattern
-@goal @glob '*.txt' should_not_have_anything_after_glob_pattern
-@goal @glob '*.txt' # but comment is OK
+#@goal @glob '*.txt' should not have anything after glob pattern
+#@goal @glob '*.txt' should_not_have_anything_after_glob_pattern
 
-@goal @glob     # absent glob pattern
-@goal g19 @glob # absent glob pattern
+#@goal @glob '*.txt' # but comment is OK
+#
+#@goal @glob     # absent glob pattern
+#@goal g19 @glob # absent glob pattern
