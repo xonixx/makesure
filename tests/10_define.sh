d6e88cecddca85a83b1d6394e4288cc080297c10
xonix <xonixx@gmail.com>
2023-09-18 00:29:39 +0300

    Spaces in `@define` are not recognized properly #142

--- tests/10_define.sh
+++ tests/10_define.sh
@@ -42,3 +42,7 @@ echo C=$C
 @goal test_commented_define
   echo "in goal: $Commented"
   sh -c 'echo in child process: $Commented'
+
+@define ISSUE_142='a     b' # 5 spaces
+@goal issue142
+  echo "$ISSUE_142"
