6ecc4c09c40b14e2d26fbb29beb26552b06310f8
xonix <xonixx@gmail.com>
2024-02-24 17:52:55 +0200

    Allow glob to be interpolated string to facilitate reuse #165

---
+++ tests/31_glob_interpolated.sh
@@ -0,0 +1,15 @@
+
+
+@goal g1 @glob "glob_test/*.txt"
+  echo "$ITEM"
+
+@define G='glob_test'
+
+@goal g2 @glob "$G/*.txt"
+  echo "$ITEM"
+
+@define G1="$G/*.txt"
+
+@goal g3 @glob "$G1"
+  echo "$ITEM"
+
