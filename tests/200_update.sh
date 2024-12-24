05ae458a33c6d71cbcc74f937e7800e6a391375d
xonixx <xonixx@gmail.com>
2024-12-25 00:33:28 +0200

    selfupdate is broken #174 : adjust/fix selfupdate test

--- tests/200_update.sh
+++ tests/200_update.sh
@@ -8,16 +8,6 @@
 @goal env_prepared
   [[ -d "$D" ]] && rm -r "$D"
   mkdir "$D"
-#  cat "$D/$MAKESURE"; exit
-
-#  export NEXT_VERSION=0.9.22 # TODO calc by subtracting 1
-#  if [[ "$("$D/$MAKESURE" --version)" != "$NEXT_VERSION" ]]
-#  then
-#    # this is compiled version
-#    awk '/^exec/ { sub(/Version=[0-9.]+/,"Version=XXX") } 1' "$D/$MAKESURE" > "$D/$MAKESURE"_1
-#    cat "$D/$MAKESURE"_1 > "$D/$MAKESURE"
-#    rm "$D/$MAKESURE"_1
-#  fi
 
   for cmd in awk mktemp rm cp dirname cat chmod
   do
@@ -71,25 +61,6 @@
 @depends_on curl_prepared
 @use_lib
   run_selfupdate
-
-# TODO is it possible to test via native wget if available?
-#@goal wget_prepared
-#@depends_on env_prepared
-#  cmd="wget"
-#
-#  echo $'#!/bin/sh
-#echo "running wget"
-## fake wget with curl
-#exec awk -v CURL="'$(command -v curl)$'" -v a1="$1" -v a2="$2" -v a3="$3" \'
-#BEGIN {
-#sub(/-q/,"-s",a1)
-#sub(/-O/,"-o",a3)
-##print(CURL " " a1 " " a2 " " a3)
-#system(CURL " " a1 " " a2 " " a3)
-#}\'
-#' > "$D/$cmd"
-#
-#  chmod +x "$D/$cmd"
 
 @goal wget_prepared
 @depends_on env_prepared
